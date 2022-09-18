dd188749812cf6d2b44f88fa6fd6a42934d2fa67
Vincent Agnano <vinyll@protonmail.com>
2022-09-18 13:28:17 +0200

    fixed route for tabbar

--- components/tabbar.vue
+++ components/tabbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-view />
-    <van-tabbar>
+    <van-tabbar route>
       <van-tabbar-item to="/profile" icon="manager-o">Infos</van-tabbar-item>
       <van-tabbar-item to="/pay" icon="back-top">Envoyer</van-tabbar-item>
     </van-tabbar>
