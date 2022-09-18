ee8aee60f23d1f96f538b85cad1e5ff21e873da5
Vincent Agnano <vinyll@protonmail.com>
2022-09-18 20:26:44 +0200

    connect to pocketbase and save transaction

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   css: ['vant/lib/index.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/vant'],
+  plugins: ['@/plugins/vant', '@/plugins/pocketbase'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
